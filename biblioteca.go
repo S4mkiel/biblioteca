c148e5f1df8720c7b35fad054fcf661b17b7edac
Roberto Filho <zferraz.rf@gmail.com>
2022-03-28 17:30:41 -0300

    Alteração

--- biblioteca.go
+++ biblioteca.go
@@ -1,6 +1,9 @@
 package Biblioteca
 
-import "fmt"
+import (
+	"fmt"
+	
+)
 
 type biblioteca struct{
 	Nome 	string
@@ -10,15 +13,11 @@ type biblioteca struct{
 }
 
 
-func Biblioteca(b biblioteca) string{
+func Bd(b biblioteca) string{
 	fmt.Printf("Informe os dados do livro:\nNome:%s\nAssunto:%s\nAutor:%s\nISBN:%s", b.Nome, b.Assunto, b.Autor, b.ISBN)
-	//for count := 1; count < 5; count++{
-		//if (count < 5){
-		//	fmt.Sprint("Informe os dados do livro:\nNome:%s\nAssunto:%s\nAutor:%s\nISBN:%s", b.Nome, b.Assunto, b.Autor, b.ISBN)
-		//}else{
-	//		fmt.Print("O número maximo de adição já foi batido")
-	//	}
-//	}
 	return b.Nome
+}
 
+func internal() bool{
+	return true
 }
